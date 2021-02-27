f1ac523bab7765187e9d5c0ce0c2b787d740e7c5
allanwzhang <33790915+allanwzhang@users.noreply.github.com>
2021-02-26 22:12:03 -0500

    Add files via upload

--- AStarAlg.java
+++ AStarAlg.java
@@ -8,8 +8,8 @@ public class AStarAlg {
 	public static void main(String[] args) throws IOException {
 		int[][] graph = load("C:\\Allan\\intermediate\\testGraph.csv");
 		int[][] nearest= load("C:\\Allan\\intermediate\\nearestGraph.csv");
-		
-		Grid last = findPath(13, 19, 607, 1010, graph, nearest);
+
+		Grid last = findPath(489, 402, 569, 313, graph, nearest);
 		
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Allan\\intermediate\\Path.csv")));
 		int count = 0;
@@ -26,7 +26,15 @@ public class AStarAlg {
 	static Grid findPath(int sx, int sy, int ex, int ey, int[][] grid, int[][] nearest) {
 		if(grid[sx][sy] != 0 || grid[ex][ey] != 0) return null;
 		
-		PriorityQueue<Grid> pq = new PriorityQueue<>(10, (Grid a, Grid b) -> a.score - b.score);
+		double totalDist = Math.abs(sx-ex) + Math.abs(sy-ey);
+		double maxNearest = 10.0;
+		
+		double WDIST = 0.5;
+		double WNEAR = 0.3;
+		double WTURN = 0.2;
+		
+		Comparator<Grid> comparator = new GridComparator();
+		PriorityQueue<Grid> pq = new PriorityQueue<>(10, comparator);
 		pq.add(new Grid(sx, sy, ' ', 0, 0, null));
 		
 		boolean[][] visited = new boolean[630][1030];
@@ -37,34 +45,34 @@ public class AStarAlg {
 			visited[curr.x][curr.y] = true;
 			
 			if(curr.x > 0 && !visited[curr.x-1][curr.y] && grid[curr.x-1][curr.y] == 0) {
-				int eucDist =(int) Math.sqrt(Math.pow(ex-(curr.x-1), 2)+Math.pow(ey-curr.y, 2));
-				int nextScore = eucDist + curr.dist+1 + 100*(10-nearest[curr.x-1][curr.y]);
-				//change this ratio ^
-				if(curr.dir=='U'||curr.dir=='D') nextScore += 100;
+				int manDist = Math.abs(curr.x-1-ex) + Math.abs(curr.y-ey);
+				int turn = 0;
+				if(curr.dir=='U'||curr.dir=='D') turn = 1;
+				double nextScore = ((manDist+curr.dist+1.0)/(totalDist*2))*WDIST + ((maxNearest-nearest[curr.x-1][curr.y])/maxNearest)*WNEAR + turn*WTURN;   
 				pq.add(new Grid(curr.x-1, curr.y, 'L', nextScore, curr.dist+1, curr));
 			}
 			
 			if(curr.x+1 < grid.length && !visited[curr.x+1][curr.y] && grid[curr.x+1][curr.y] == 0) {
-				int eucDist =(int) Math.sqrt(Math.pow(ex-(curr.x+1), 2)+Math.pow(ey-curr.y, 2));
-				int nextScore = eucDist + curr.dist+1 + 100*(10-nearest[curr.x+1][curr.y]);
-				//change this ratio ^
-				if(curr.dir=='U'||curr.dir=='D') nextScore += 100;
+				int manDist = Math.abs(curr.x+1-ex) + Math.abs(curr.y-ey);
+				int turn = 0;
+				if(curr.dir=='U'||curr.dir=='D') turn = 1;
+				double nextScore = ((manDist+curr.dist+1.0)/(totalDist*2))*WDIST + ((maxNearest-nearest[curr.x+1][curr.y])/maxNearest)*WNEAR + turn*WTURN;
 				pq.add(new Grid(curr.x+1, curr.y, 'R', nextScore, curr.dist+1, curr));
 			}
 			
 			if(curr.y > 0 && !visited[curr.x][curr.y-1] && grid[curr.x][curr.y-1] == 0) {
-				int eucDist =(int) Math.sqrt(Math.pow(ex-curr.x, 2)+Math.pow(ey-(curr.y-1), 2));
-				int nextScore = eucDist + curr.dist+1 + 100*(10-nearest[curr.x][curr.y-1]);
-				//change this ratio ^
-				if(curr.dir=='L'||curr.dir=='R') nextScore += 100;
+				int manDist = Math.abs(curr.x-ex) + Math.abs(curr.y-1-ey);
+				int turn = 0;
+				if(curr.dir=='L'||curr.dir=='R') turn = 1;
+				double nextScore = ((manDist+curr.dist+1.0)/(totalDist*2))*WDIST + ((maxNearest-nearest[curr.x][curr.y-1])/maxNearest)*WNEAR + turn*WTURN;
 				pq.add(new Grid(curr.x, curr.y-1, 'D', nextScore, curr.dist+1, curr));
 			}
 			
 			if(curr.y+1 < grid[0].length && !visited[curr.x][curr.y+1] && grid[curr.x][curr.y+1] == 0) {
-				int eucDist =(int) Math.sqrt(Math.pow(ex-curr.x, 2)+Math.pow(ey-(curr.y+1), 2));
-				int nextScore = eucDist + curr.dist+1 + 100*(10-nearest[curr.x][curr.y+1]);
-				//change this ratio ^
-				if(curr.dir=='L'||curr.dir=='R') nextScore += 100;
+				int manDist = Math.abs(curr.x-ex) + Math.abs(curr.y+1-ey);
+				int turn = 0;
+				if(curr.dir=='L'||curr.dir=='R') turn = 1;
+				double nextScore = ((manDist+curr.dist+1.0)/(totalDist*2))*WDIST + ((maxNearest-nearest[curr.x][curr.y+1])/maxNearest)*WNEAR + turn*WTURN;
 				pq.add(new Grid(curr.x, curr.y+1, 'U', nextScore, curr.dist+1, curr));
 			}
 		}
@@ -72,14 +80,24 @@ public class AStarAlg {
 		return null;
 	}
 	
+	static public class GridComparator implements Comparator<Grid> {
+	    @Override
+	    public int compare(Grid x, Grid y) {
+	    	if(x.score < y.score) return -1;
+	    	if(x.score > y.score) return 1;
+	    	return 0;
+	    }
+	    
+	}
+	
 	static class Grid {
 		int x,y;
 		char dir;
-		int score;
+		double score;
 		int dist;
 		Grid prev;
 		
-		public Grid(int xx, int yy, char d, int s, int di, Grid p) {
+		public Grid(int xx, int yy, char d, double s, int di, Grid p) {
 			x=xx;
 			y=yy;
 			dir=d;
